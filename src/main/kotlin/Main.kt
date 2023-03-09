7dc386d1315a0b127472dcff75870727f4ec7d77
Maria Pshenichinkova <annaka4356@gmail.com>
2023-03-09 20:07:23 +0300

    Task5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.log
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    println("Введите целое число: ")
+    val n = readln().toIntOrNull()
+    println("Целое основание:")
+    val f = readln().toIntOrNull();
+
+    if (f == 0) {
+        if (n == 1) {
+            println("0")
+            return
+        } else if (n == 0) {
+            println("1")
+            return
+        }
+    }
+
+    if (n != null && f != null) {
+        if (!log(n.toFloat(), f.toFloat()).rem(1).equals(0.0f)) {
+            println("Подходящего целого числа нет..");
+        } else {
+            println(log(n.toFloat(), f.toFloat()).toInt())
+        }
+    } else {
+        println("Ошибка во вводе чисел..")
+    }
 }
